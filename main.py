9f47f90df1d474a026f216b345c48c7d6dcf3729
Adil S <forever@nzironeed.ru>
2023-05-14 12:02:32 +0500

    expanded code in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from employees.class_hh import HHManager
 from vacancies.class_vacancy import VacancyManager
 from utils.user_input import user_input
+from db_classes.builder import DBBuilder
+from db_classes.manager import DBManager
 
 
 if __name__ == '__main__':
@@ -11,6 +13,14 @@ if __name__ == '__main__':
 
     companies_list = user_input()
 
+    db_builder = DBBuilder(input("Введите название базы данных"))
+    db_manager = DBManager()
+
+    db_builder.create_database()
+    db_builder.create_tables()
+
+    db_name = db_builder.name
+
     for company in companies_list:
         employee_data = hh_manager.get_employee_data(company)
         employee_id = hh_manager.get_employee_id(employee_data)
@@ -18,3 +28,8 @@ if __name__ == '__main__':
 
         vacancy_data = vacancy_manager.get_vacancy_data(employee_id)
         vacancy_manager.save_as_csv(vacancy_data)
+
+        employees_data = db_manager.get_csv() #Путь до емплоес
+        vacancies_data = db_manager.get_csv() #Путь до вакансиес
+
+        db_manager.insert_data(db_name, employees_data, vacancies_data)
